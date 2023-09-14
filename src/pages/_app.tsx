bab653503b3392ce487b381e1f8ea825c59cba82
facubozzi <facundo.bo@hotmail.com>
2023-09-13 21:29:17 -0300

    change: title of the page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
 import Layout from "@/pages/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Rather Labs Challenge</title>
+      </Head>
+
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </QueryClientProvider>
+    </>
   );
 }
